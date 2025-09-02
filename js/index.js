68d84d5c8570096f364d9be2cfccc6e13cb4ccc5
soumojit@2000 <soumojitdutta0542@gmail.com>
2025-09-02 15:21:16 +0530

    Updated HTML and Js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,50 @@
-let navbar=document.getElementById('navbar');
+let navbar = document.getElementById("navbar");
 
-window.addEventListener('scroll', checkScroll);
+window.addEventListener("scroll", checkScroll);
 
-function checkScroll(){
-    if (window.scrollY>100){
-        navbar.classList.add('scrolled');
-    }
-    else{
-        navbar.classList.remove('scrolled');
-    }
+function checkScroll() {
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 }
 
 const searchIcon = document.getElementById("searchIcon");
-    const searchBox = document.getElementById("searchBox");
+const searchBox = document.getElementById("searchBox");
 
-    // Named function
-    function toggleSearchBox() {
-      if (searchBox.style.display === "block") {
-        searchBox.style.display = "none";
-      } else {
-        searchBox.style.display = "block";
-        searchBox.querySelector("input").focus();
-      }
-    }
+// Named function
+function toggleSearchBox() {
+  if (searchBox.style.display === "block") {
+    searchBox.style.display = "none";
+  } else {
+    searchBox.style.display = "block";
+    searchBox.querySelector("input").focus();
+  }
+}
 
-    // Event listener
-    searchIcon.addEventListener("click", toggleSearchBox);
+// Event listener
+searchIcon.addEventListener("click", toggleSearchBox);
+
+//video-player
+document.addEventListener("DOMContentLoaded", function () {
+  const thumb = document.getElementById("playVideo");
+
+  thumb.addEventListener("click", function () {
+    // bringing iframe
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://www.youtube.com/embed/kcfs1-ryKWE?autoplay=1&rel=0&modestbranding=1";
+    iframe.width = "1360";
+    iframe.height = "550";
+    iframe.title = "YouTube video player";
+    iframe.frameBorder = "0";
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowFullscreen = true;
+    iframe.style = "aspect-ratio:16/9; border-radius:12px; margin-top:90px";
+
+    // replacing image with iframe
+    thumb.replaceWith(iframe);
+  });
+});
+
